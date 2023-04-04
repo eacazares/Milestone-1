f7d59c27abef0d717564a585e38bf72c94b9aef4
eacazares <93634691+eacazares@users.noreply.github.com>
2023-04-04 16:30:49 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const triviaQuestions = document.getElementById("questions-container");
+
+
+const question1 = {
+    question: "hello"
+    options
+}
+   
+        
+function showQuestion(qs){
+    triviaQuestions.innerText = qs.question
+}
+
+function showtriviaOptions(opts){
+    triviaOptions.innerText = opts.option1
+}
+showQuestion(question1)
+showtriviaOptions(question1)
